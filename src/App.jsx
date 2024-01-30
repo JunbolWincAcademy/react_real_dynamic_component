c38281cf93964aaf623fd04632e818cfa81f221c
Junier Bolivar <junierba@yahoo.co.uk>
2024-01-30 20:09:29 +0100

    add text to files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-// import React, { useState } from 'react'; // this line should always be there
-// import reactLogo from './assets/react.svg';
-import { DrinkButtons } from './components/ui/DrinkButtons'; //I forgot to write the whole path to the file
-import './App.css';
+import './App.css';// this is different
+import { DrinkButtons } from './components/DrinkButtons';//this is different DrinkButtons is not in the ui folder
+//well I dont know why it was in the ui folder before
 
 export const App = () => {
-  const welcomeGreeting = 'Welcome to our Coffee & Thee Corner!';
+  const greeting = 'Welcome to our cafe!';
 
   return (
-    <>
-      <div>
-        <img src="/assets/coffee_and_thee_logo_2.png" className="logo" alt="Coffee & Tee logo" />
-        <h1 className="mainTitle">{welcomeGreeting}</h1>
-        <DrinkButtons drinkOne={'Coffee'} drinkTwo={'Thee'} />
-        {/* <DrinkButtons /> */}
-      </div>
-    </>
+    <div className="App">
+      <h1>{greeting}</h1>
+      <DrinkButtons /> {/*this was commented before remember to use {....} and only /*....  inside them  */} 
+    </div>
   );
 };
+
+
+
+
+
+//--------------OLD CODE------------------
+
+// import React, { useState } from 'react'; // this line should always be there
+// import reactLogo from './assets/react.svg';
+// import { DrinkButtons } from './components/ui/DrinkButtons'; //I forgot to write the whole path to the file?*
+// import './App.css';
+
+// export const App = () => {
+//   const welcomeGreeting = 'Welcome to our Coffee & Thee Corner!';
+
+//   return (
+//     <>
+//       <div>
+//         <img src="/assets/coffee_and_thee_logo_2.png" className="logo" alt="Coffee & Tee logo" />
+//         <h1 className="mainTitle">{welcomeGreeting}</h1>
+//         <DrinkButtons drinkOne={'Coffee'} drinkTwo={'Thee'} />
+//         {/* <DrinkButtons /> */}
+//       </div>
+//     </>
+//   );
+// };
 
--- src/components/ui/DrinkButtons.jsx
+++ src/components/ui/DrinkButtons.jsx
@@ -1,30 +1,45 @@
-// export const DrinkButtons = (props) => {// here props is a parameter
+import './DrinkButtons.css';
 
-
-export const DrinkButtons = ({ drinkOne, drinkTwo }) => {
-  // here props is a parameter
-  // before it was no parameter
-  const Choose = 'Hello!, would you like coffee or thee?';
-  // const tea = 'Tea';
-  // const coffee = 'Coffee';
-
+export const DrinkButtons = () => {
   return (
     <>
-      <div className="DrinkButtons">
-        <h2>{Choose}</h2>
-      </div>
+      <h2>Would you like tea or coffee?</h2>
       <div className="button-group">
-        <button className="btn_thee">{drinkOne}</button>
-        <button className="btn_coffee">{drinkTwo}</button>
-
+        <button className="button">Tea</button>
+        <button className="button">Coffee</button>
       </div>
     </>
-    //     {/* <button className="btn_thee">{props.drinkOne}</button>
-    //     <button className="btn_coffee">{props.drinkTwo}</button> */}
-
-    //     {/* <button className="btn_thee"><p className= "btn_color">{tea}</p></button>
-    //     <button className="btn_coffee"><p className= "btn_color">{coffee}</p></button> */}
-    //   </div>
-    // </>
   );
 };
+
+//---------------- this was the old code
+
+// export const DrinkButtons = (props) => {// here props is a parameter
+
+// export const DrinkButtons = ({ drinkOne, drinkTwo }) => {
+//   // here props is a parameter
+//   // before it was no parameter
+//   const Choose = 'Hello!, would you like coffee or thee?';
+//   // const tea = 'Tea';
+//   // const coffee = 'Coffee';
+
+//   return (
+//     <>
+//       <div className="DrinkButtons">
+//         <h2>{Choose}</h2>
+//       </div>
+//       <div className="button-group">
+//         <button className="btn_thee">{drinkOne}</button>
+//         <button className="btn_coffee">{drinkTwo}</button>
+
+//       </div>
+//     </>
+//     //     {/* <button className="btn_thee">{props.drinkOne}</button>
+//     //     <button className="btn_coffee">{props.drinkTwo}</button> */}
+
+//     //     {/* <button className="btn_thee"><p className= "btn_color">{tea}</p></button>
+//     //     <button className="btn_coffee"><p className= "btn_color">{coffee}</p></button> */}
+//     //   </div>
+//     // </>
+//   );
+// };
