1b5bd554636c24ac30dd83331d11864230dbe4bc
Junier Bolivar <junierba@yahoo.co.uk>
2024-02-01 22:00:39 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,35 +15,27 @@ export const App = () => {
 //While writing the object names, auto import the objects, or manually import them.
 
 //Create a new const userDrink and assign it the tea or coffee object.
-const userDrink = tea;
+const userDrink = null;
   return (
     <div className="App">
-      <h1>{greeting}</h1>
-      <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
-      <DrinkChoice drink={userDrink} /> {/* Passing userDrink as the drink prop */}
-     { /*<DrinkButtons /> }{/*this was commented before remember to use {....} and only /*....  inside them  */}
+      {userDrink 
+        ? (
+            // Only render DrinkChoice if a drink is selected
+              <DrinkChoice drink={userDrink} />
+          )
+        : (
+            <>
+              <h1>{greeting}</h1>
+              <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
+              <p>Please select a drink</p>
+            </>
+          )
+      }
     </div>
   );
 };
 
-//--------------OLD CODE------------------
+// NOTES:
 
-// import React, { useState } from 'react'; // this line should always be there
-// import reactLogo from './assets/react.svg';
-// import { DrinkButtons } from './components/ui/DrinkButtons'; //I forgot to write the whole path to the file?*
-// import './App.css';
-
-// export const App = () => {
-//   const welcomeGreeting = 'Welcome to our Coffee & Thee Corner!';
-
-//   return (
-//     <>
-//       <div>
-//         <img src="/assets/coffee_and_thee_logo_2.png" className="logo" alt="Coffee & Tee logo" />
-//         <h1 className="mainTitle">{welcomeGreeting}</h1>
-//         <DrinkButtons drinkOne={'Coffee'} drinkTwo={'Thee'} />
-//         {/* <DrinkButtons /> */}
-//       </div>
-//     </>
-//   );
-// };
+// The use of parentheses () is particularly emphasized in scenarios involving conditional rendering with the ternary operator in JSX, 
+// especially when the expressions span multiple lines or are complex.
